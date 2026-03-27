ac23546766d5042785b19dfaa7d6dc62082b529f
xanderjl <alexanderjameslow@gmail.com>
2026-03-27 14:46:11 -0700

    chore: inline function types

--- packages/api/src/utils/auth.ts
+++ packages/api/src/utils/auth.ts
@@ -1,12 +1,13 @@
 import { SignJWT } from "jose";
 import type { User } from "~/generated/prisma/client";
 
-type CreateJWT = (user: User, secret: string) => Promise<string>;
-
 export const encodeSecret = (secret: string) =>
   new TextEncoder().encode(secret);
 
-export const createJWT: CreateJWT = async (user, secret) => {
+export const createJWT = async (
+  user: User,
+  secret: string,
+): Promise<string> => {
   const encodedSecret = encodeSecret(secret);
   const jwt = await new SignJWT(user)
     .setProtectedHeader({ alg: "HS256" })
