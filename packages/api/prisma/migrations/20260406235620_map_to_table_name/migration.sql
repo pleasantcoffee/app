cab68a56382f3983a47b849e2a42ef20da1a4904
xanderjl <alexanderjameslow@gmail.com>
2026-04-06 16:56:54 -0700

    feat: add follows table

---
+++ packages/api/prisma/migrations/20260406235620_map_to_table_name/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Follows` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_followerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_followingId_fkey";
+
+-- DropTable
+DROP TABLE "Follows";
+
+-- CreateTable
+CREATE TABLE "follows" (
+    "followerId" INTEGER NOT NULL,
+    "followingId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "follows_pkey" PRIMARY KEY ("followerId","followingId")
+);
+
+-- AddForeignKey
+ALTER TABLE "follows" ADD CONSTRAINT "follows_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "follows" ADD CONSTRAINT "follows_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
